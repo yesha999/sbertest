7253f58d2236a7b4583c8c889c6b45c6745c3efa
yesha <scrolltrip@mail.ru>
2022-11-30 16:01:27 +0300

    Добавлена сортировка в таск3

--- pure_sql/task3.sql
+++ pure_sql/task3.sql
@@ -1,6 +1,6 @@
-SELECT people.id, people.name, COUNT(sales.id) as sale_count, SUM(sales.price) as sale_rank  FROM sbertest.dbo.people AS people
+SELECT people.id, people.name, COUNT(sales.id) as sale_count, SUM(sales.price) as sale_rank FROM sbertest.dbo.people AS people
+
 LEFT JOIN sbertest.dbo.sales 
 ON people.id = sales.people_id
-GROUP BY people.id, people.name;
-
-SELECT COUNT(sales.id) as TOTAL_SALES_COUNT FROM sbertest.dbo.sales
+GROUP BY people.id, people.name
+ORDER BY sale_count DESC;
