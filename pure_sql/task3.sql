5ccfbb00cc48e887384e69f775dcf8089cb92e3a
yesha <scrolltrip@mail.ru>
2022-11-30 16:12:14 +0300

    Добавлено ранжирование

--- pure_sql/task3.sql
+++ pure_sql/task3.sql
@@ -1,6 +1,6 @@
-SELECT people.id, people.name, COUNT(sales.id) as sale_count, ROW_NUMBER() over (order by COUNT(sales.id) desc) as sale_rank FROM sbertest.dbo.people AS people
+SELECT people.id, people.name, COUNT(sales.id) as sale_count, ROW_NUMBER() over (ORDER BY COUNT(sales.id) desc) as sale_rank FROM sbertest.dbo.people AS people
 
 LEFT JOIN sbertest.dbo.sales 
 ON people.id = sales.people_id
 GROUP BY people.id, people.name
-ORDER BY sale_count DESC;
+ORDER BY sale_rank;
